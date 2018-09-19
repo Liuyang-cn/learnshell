541093a6d574492b0d2c22f1a1fb02fe44e56545
ly <woaiwojia2013@aliyun.com>
2018-09-19 11:27:53 +0000

    if-then-else.sh

--- 11/if-then.sh
+++ 11/if-then.sh
@@ -11,3 +11,11 @@ then
 fi
 echo "we are outside of the if statement"
 
+#testing multiple commands in the then section
+testuser=ly
+if grep $testuser /etc/passwd
+then
+     echo The bash files for user $testuser are:
+     ls -a /home/$testuser/.b*
+fi
+
