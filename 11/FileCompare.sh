6a88ba49ba503edb68a1ec0ebdfde94c6d61e9e3
ly <woaiwojia2013@aliyun.com>
2018-10-01 08:51:06 +0000

    filecompare

--- 11/FileCompare.sh
+++ 11/FileCompare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-##
+#checking if the name exist as a directory
 if [ -d $HOME ]
 then
     echo " Your HOME directory exists"
@@ -8,3 +8,22 @@ then
 else
     echo "There is a problem with your HOME directory"
 fi
+##
+###checking if a directory exists
+if [ -e ~ ]
+then
+    echo "OK on the directory.now to check the file"
+    # checking if a file exists
+    if [ -e ~/shell/11/testing ]
+    then
+       # the file exists,append data to it.
+        echo "Appending date to existing file testing"
+        date >> ~/shell/11/testing
+    else 
+       # creat the file,and add data to it.
+        echo "creating new file"
+        date > ~/shell/11/testing
+    fi
+else
+    echo "Sorry,you do not have a ~ directory"
+fi 
